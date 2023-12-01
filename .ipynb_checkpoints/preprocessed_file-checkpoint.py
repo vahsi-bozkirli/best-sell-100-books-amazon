a3874270635d9b02b08ded22461288b606d2a01b
tunadags <matyknv_mst@outlook.com>
2023-12-01 12:54:15 +0300

    files upgraded

--- .ipynb_checkpoints/preprocessed_file-checkpoint.py
+++ .ipynb_checkpoints/preprocessed_file-checkpoint.py
@@ -2,18 +2,38 @@ import pandas as pd
 
 # Data class for ETL process
 class Data:
-    def __init__(self,path_books,path_cust):
-        self.top_books = pd.read_csv(path_books)
-        self.custrew = pd.read_csv(path_cust)
-        
-    def preprared_data(self):
-        if "Rank" in self.top_books.columns and "Sno" in self.custrew.columns:
-            self.top_books.set_index("Rank",inplace=True)
-            self.custrew.drop("Sno",axis=1,inplace=True)
-            return self.top_books,self.custrew
-        else:
-            return self.top_books,self.custrew
+    def __init__(self, path_books, path_cust):
+        self.path_books = path_books
+        self.path_cust = path_cust
+        self._prepared = False  # Flag to indicate if data is prepared
+        self._top_books = None
+        self._custrew = None
 
+    @property
+    def prepared(self):
+        return self._prepared
+
+    def prepare_data(self):
+        self._top_books = pd.read_csv(self.path_books)
+        self._custrew = pd.read_csv(self.path_cust)
+        
+        if "Rank" in self._top_books.columns and "Sno" in self._custrew.columns:
+            self._top_books.set_index("Rank", inplace=True)
+            self._custrew.drop("Sno", axis=1, inplace=True)
+            self._prepared = True
+
+    @property
+    def top_books(self):
+        if not self.prepared:
+            self.prepare_data()
+        return self._top_books
+
+    @property
+    def custrew(self):
+        if not self.prepared:
+            self.prepare_data()
+        return self._custrew
+    
     def added_features(self):
         """
         this method adds features to top_books and custrew datas
@@ -22,6 +42,9 @@ class Data:
         
         # object type columns content applied str.lower method
         # first loop is for top books
+        if not self.prepared:
+            self.prepare_data()
+                    
         for col in self.top_books.select_dtypes(include=[object]).columns:
             self.top_books[col] = self.top_books[col].str.lower()
 
--- model/read_data.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-def import_py_file(path):
-    sys.path.append(path)
